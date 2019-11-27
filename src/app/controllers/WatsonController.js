70c61165c1f30e4fef1ad5476c35e30f84ae7ee6
lliuti <lucas_liuti@hotmail.com>
2019-11-27 10:48:50 -0300

    Updating WatsonController

--- src/app/controllers/WatsonController.js
+++ src/app/controllers/WatsonController.js
@@ -4,111 +4,24 @@ class WatsonController {
   async store(req, res) {
     const { text, context = {}, counter } = req.body;
     let username = '';
-
-    if (counter == 0) {
-      username = text;
-      
-      console.log(username);
-
-      const createSession = await watson.createSession({
-        assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c'
-      });
-      const sessionId = createSession.result.session_id;
-  
-      const params = {
-        input: { 
-          'message_type': 'text',
-          'text': '', 
-        }, 
-        context: {
-          skills: {
-            'main skill': {
-              'user_defined': {
-                'username': username,
-              }
-            }
-          }
-        },
-        assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c',  
-        sessionId,
-      }
-  
-      watson.message(params, (err, response) => {
-        if (err) { 
-          res.status(500).json({ error: err });
-        }
-        return res.status(200).json(response);
-      });
-    } 
-
+    let userInput = '';
     switch (counter) {
+      case 0:
+        username = text;
+        userInput = '';
+        break; 
       case 1:
-        const createSession = await watson.createSession({
-          assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c'
-        });
-        const sessionId = createSession.result.session_id;
-    
-        const params = {
-          input: { 
-            'message_type': 'text',
-            'text': 'Quero saber mais sobre programação', 
-          }, 
-          context: {
-            skills: {
-              'main skill': {
-                'user_defined': {
-                  'username': username,
-                }
-              }
-            }
-          },
-          assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c',  
-          sessionId,
-        }
-    
-        watson.message(params, (err, response) => {
-          if (err) { 
-            res.status(500).json({ error: err });
-          }
-          return res.status(200).json(response);
-        });
-
-      case 2: {
-        const createSession = await watson.createSession({
-          assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c'
-        });
-        const sessionId = createSession.result.session_id;
-    
-        const params = {
-          input: { 
-            'message_type': 'text',
-            'text': 'O Brasil tem programadores bons?', 
-          }, 
-          context: {
-            skills: {
-              'main skill': {
-                'user_defined': {
-                  'username': username,
-                }
-              }
-            }
-          },
-          assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c',  
-          sessionId,
-        }
-    
-        watson.message(params, (err, response) => {
-          if (err) { 
-            res.status(500).json({ error: err });
-          }
-          return res.status(200).json(response);
-        });
-      }
+        userInput = 'Quero saber mais sobre programação';
+        break;
+      case 2:
+        userInput = 'O Brasil tem programadores bons?';
+        break;
+      case 3:  
+        userInput = 'O mercado tem vagas disponíveis?';
+        break;
+      default:
+        break;
     }
-
-    // if (!text) {
-    //   return res.status(401).json({ error: 'Cannot answer that' });
-    // }
 
     const createSession = await watson.createSession({
       assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c'
@@ -118,7 +31,7 @@ class WatsonController {
     const params = {
       input: { 
         'message_type': 'text',
-        'text': text, 
+        'text': userInput, 
       }, 
       context: {
         skills: {
@@ -132,10 +45,10 @@ class WatsonController {
       assistantId: '1926dd97-a05a-4d6f-8a8d-8a94f3494d2c',  
       sessionId,
     }
-
     watson.message(params, (err, response) => {
-      if (err) 
-        res.status(500).json({ error: err });
+      if (err) { 
+        return res.status(500).json({ error: err });
+      }
       return res.json(response);
     });
   }
